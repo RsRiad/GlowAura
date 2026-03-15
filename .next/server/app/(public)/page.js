d8d54b92af4ebdc87e894cc7e035fb3f9cdb4ac1
RsRiad <rsriad00@gmail.com>
2026-03-16 01:19:28 +0600

    added inngest

--- .next/server/app/(public)/page.js
+++ .next/server/app/(public)/page.js
@@ -1,9 +1,9 @@
 const CHUNK_PUBLIC_PATH = "server/app/(public)/page.js";
 const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_0962b827._.js");
-runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__4c58c055._.js");
+runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__fb250061._.js");
 runtime.loadChunk("server/chunks/ssr/app_1f3630ef._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_2e0a5974._.js");
+runtime.loadChunk("server/chunks/ssr/node_modules_8e3e5b6d._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_62645a6b._.js");
 runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__4c2bd7af._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_96715ba7._.js");
