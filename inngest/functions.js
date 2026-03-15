62badb70900e5e61d49f8125b40aeb60a6eee7e0
RsRiad <rsriad00@gmail.com>
2026-03-15 19:03:11 +0600

    added inngest

---
+++ inngest/functions.js
@@ -0,0 +1,58 @@
+import { inngest } from "./client";
+import prisma from "../lib/prisma";
+
+export const syncUserCreation = inngest.createFunction(
+    { id: "sync-user-creation" },
+    { event: "clerk/user.created" },
+    async ({ event, step }) => {
+        const { data } = event;
+        const { id, email_addresses, first_name, last_name, image_url } = data;        
+        const email = email_addresses[0].email_address;
+        const name = `${first_name} ${last_name}`;
+        const image = image_url;
+        await prisma.user.create({
+            data: {
+                id,
+                email,
+                name,
+                image,
+            },
+        });
+    },
+);
+
+export const syncUserUpdate = inngest.createFunction(
+    { id: "sync-user-update" },
+    { event: "clerk/user.updated" },
+    async ({ event, step }) => {
+        const { data } = event;
+        const { id, email_addresses, first_name, last_name, image_url } = data;
+        const email = email_addresses[0].email_address;
+        const name = `${first_name} ${last_name}`;
+        const image = image_url;
+        await prisma.user.update({
+            where: {
+                id,
+            },
+            data: {
+                email,
+                name,
+                image,
+            },
+        });
+    },
+);
+
+export const syncUserDelete = inngest.createFunction(
+    { id: "sync-user-delete" },
+    { event: "clerk/user.deleted" },
+    async ({ event, step }) => {
+        const { data } = event;
+        const { id } = data;
+        await prisma.user.delete({
+            where: {
+                id,
+            },
+        });
+    },
+);
